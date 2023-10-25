de5bd09b0c8df60fbf07a1d93fff9048b6cdc183
LuzielmaSilva <luzielma.silva@yahoo.com.br>
2023-10-24 22:51:53 -0300

    Uso do Log

--- Petshop/PetShop.java
+++ Petshop/PetShop.java
@@ -10,146 +10,148 @@ public class PetShop {
         ArrayList<Gato> listaGatos = new ArrayList<>();
         ArrayList<Agenda> listaAgendamentos = new ArrayList<>();
         
-        
+        Log meuLogger = new Log("Log.txt");
         //aqui o usuário digita os dados para cadastro ou S para sair
         Scanner scanner = new Scanner(System.in);
         while (true) {
-        System.out.println("Cadastro dos clientes Petshop Amigo Fiel. " );
-        System.out.println("Digite o nome do animal(ou S para sair): " );
-        String nome = scanner.nextLine();
+            System.out.println("Cadastro dos clientes Petshop Amigo Fiel. " );
+            System.out.println("Digite o nome do animal(ou S para sair): " );
+            String nome = scanner.nextLine();
         if(nome.equalsIgnoreCase("S")){
-            break;
+        break;
         }
-        System.out.println("Digite a especie: ");
-        String especie = scanner.nextLine();
-        System.out.println("Digite a raça: ");
-        String raca = scanner.nextLine();
-        System.out.println("Digite a data de nascimento: ");
-        String dataNascimento = scanner.nextLine();
-        System.out.println("Digite o nome do proprietário: ");
-        String proprietario = scanner.nextLine();
-        System.out.println("Digite o telefone para contato: ");
-        String telefone = scanner.nextLine();
+            System.out.println("Digite a especie: ");
+            String especie = scanner.nextLine();
+            System.out.println("Digite a raça: ");
+            String raca = scanner.nextLine();
+            System.out.println("Digite a data de nascimento: ");
+            String dataNascimento = scanner.nextLine();
+            System.out.println("Digite o nome do proprietário: ");
+            String proprietario = scanner.nextLine();
+            System.out.println("Digite o telefone para contato: ");
+            String telefone = scanner.nextLine();
         
-        System.out.println("=======================");
+            System.out.println("=======================");
     
-        //alguns serviços do Petshop
-        System.out.println("=====Nossos Serviços:====== ");
-        Banho banho = new Banho();
-        banho.descricao();
-        banho.preco();
-        Tosa tosa = new Tosa();
-        tosa.descricao();
-        tosa.preco();
-        ConsultaVeterinaria consultaVet = new ConsultaVeterinaria();
-        consultaVet.descricao();
-        consultaVet.preco();
-        BanhoETosa banhoTosa = new BanhoETosa();
-        banhoTosa.descricao();
-        banhoTosa.preco();
-        //aqui o usuário escolhe o tipo de serviço
-        System.out.println("Banho: " + banho.preco());
-        System.out.println("Tosa: "  + tosa.preco());
-        System.out.println("Consulta Veterinária: "  + consultaVet.preco());
-        System.out.println("Banho e tosa: " + banhoTosa.preco());
-        System.out.println("===========================");
-        System.out.println("Digite o serviço escolhido:" );
-        String servicos = scanner.nextLine();
+            //alguns serviços do Petshop
+            System.out.println("=====Nossos Serviços:====== ");
+            Banho banho = new Banho();
+            banho.descricao();
+            banho.preco();
+            Tosa tosa = new Tosa();
+            tosa.descricao();
+            tosa.preco();
+            ConsultaVeterinaria consultaVet = new ConsultaVeterinaria();
+            consultaVet.descricao();
+            consultaVet.preco();
+            BanhoETosa banhoTosa = new BanhoETosa();
+            banhoTosa.descricao();
+            banhoTosa.preco();
+            //aqui o usuário escolhe o tipo de serviço
+            System.out.println("Banho: " + banho.preco());
+            System.out.println("Tosa: "  + tosa.preco());
+            System.out.println("Consulta Veterinária: "  + consultaVet.preco());
+            System.out.println("Banho e tosa: " + banhoTosa.preco());
+            System.out.println("===========================");
+            System.out.println("Digite o serviço escolhido:" );
+            String servicos = scanner.nextLine();
+            meuLogger.logger.info("Servico escolhido: " + servicos);
         
-        System.out.println("===========================");
-        //agenda: aqui são incluídos os agendamentos pelo scanner
-        String cancelarAgen = "Agendamento cancelado!";
-        Agenda agendamento = new Agenda(nome,  especie, raca, dataNascimento, proprietario, telefone);
-        listaAgendamentos.add(0, agendamento);
-        agendamento.setDiaSemana(cancelarAgen);;
-        agendamento.setHorario(cancelarAgen);   
-        agendamento.setCancelamento(cancelarAgen);
+            System.out.println("===========================");
+            //agenda: aqui são incluídos os agendamentos pelo scanner
+            String cancelarAgen = "Agendamento cancelado!";
+            Agenda agendamento = new Agenda(nome,  especie, raca, dataNascimento, proprietario, telefone);
+            listaAgendamentos.add(0, agendamento);
+            agendamento.setDiaSemana(cancelarAgen);;
+            agendamento.setHorario(cancelarAgen);   
+            agendamento.setCancelamento(cancelarAgen);
         
-        //o usuário escolhe o dia e horário do agendamento
-        System.out.println("Dias disponíveis: ");
-        System.out.println("Segunda" );
-        System.out.println("Terça" );
-        System.out.println("Quarta" );
-        System.out.println("Quinta" );
-        System.out.println("Sexta" );
-        System.out.println("Sábado" );
-        System.out.println("===========================");
-        System.out.println("Horários disponíveis: ");
-        System.out.println("09:00" );
-        System.out.println("10:00" );
-        System.out.println("11:00" );
-        System.out.println("14:00" );
-        System.out.println("15:00" );
-        System.out.println("16:00" );
-        System.out.println("===========================");
-        System.out.println("Digite o dia do agendamento: ");
-        String novoDiaSemana = scanner.nextLine();
-        System.out.println("===========================");
-        System.out.println("Digite o horário: ");
-        String novoHorario = scanner.nextLine();
+            //o usuário escolhe o dia e horário do agendamento
+            System.out.println("Dias disponíveis: ");
+            System.out.println("Segunda" );
+            System.out.println("Terça" );
+            System.out.println("Quarta" );
+            System.out.println("Quinta" );
+            System.out.println("Sexta" );
+            System.out.println("Sábado" );
+            System.out.println("===========================");
+            System.out.println("Horários disponíveis: ");
+            System.out.println("09:00" );
+            System.out.println("10:00" );
+            System.out.println("11:00" );
+            System.out.println("14:00" );
+            System.out.println("15:00" );
+            System.out.println("16:00" );
+            System.out.println("===========================");
+            System.out.println("Digite o dia do agendamento: ");
+            String novoDiaSemana = scanner.nextLine();
+            meuLogger.logger.info("Dia escolhido: " + novoDiaSemana);
+            System.out.println("===========================");
+            System.out.println("Digite o horário: ");
+            String novoHorario = scanner.nextLine();
+            meuLogger.logger.info("Horário escolhido: " + novoHorario);
         
 
-        boolean sucessoAgen = agendamento.agendar(novoHorario, novoDiaSemana, listaAgendamentos);
+            boolean sucessoAgen = agendamento.agendar(novoHorario, novoDiaSemana, listaAgendamentos);
         if(sucessoAgen){
             System.out.println("Agendamento realizado!");
                 
         }else {   
-        //aqui pergunta se o usuário deseja cancelar o agen caso o horário já esteja preenchido
-        System.out.println("===========================");   
-        System.out.println("Deseja cancelar o agendamento? " );
-        System.out.println("Digite sim ou não" );
-        String cancelarAgen1  = scanner.nextLine();
-        System.out.println("Digite novamente os dados com novo horário.");
-        System.out.println("===========================");
+            //aqui pergunta se o usuário deseja cancelar o agen caso o horário já esteja preenchido
+            System.out.println("===========================");   
+            System.out.println("Deseja cancelar o agendamento? " );
+            System.out.println("Digite sim ou não" );
+            String cancelarAgen1  = scanner.nextLine();
+            System.out.println("Digite novamente os dados com novo horário.");
+            System.out.println("===========================");
         
-        agendamento.cancelar(cancelarAgen);
-        String resultadoCancelamento = agendamento.getCancelamento();
-        System.out.println(resultadoCancelamento);
+            agendamento.cancelar(cancelarAgen);
+            String resultadoCancelamento = agendamento.getCancelamento();
+            System.out.println(resultadoCancelamento);
         }
-        //aqui mesmo o agen sendo cancelado, está retornando a lista com os dados. E não é p retornar.
-        //Não consegui ajeitar! :/
-            
-        System.out.println("==========================");
-        //clientes agendados inseridos pelo scanner
-        System.out.println("Dados do agendamento:");
-         for(Agenda agendamentos : listaAgendamentos){
-         System.out.println("Animal: " + agendamentos.getNome());
-         System.out.println("Espécie: " + agendamentos.getEspecie());
-         System.out.println("Raça: " + agendamentos.getRaça());
-         System.out.println("Data nascimento: " + agendamentos.getDataNascimento());
-         System.out.println("proprietário: " + agendamentos.getProprietario());
-         System.out.println("telefone: " + agendamentos.getTelefone());
-         System.out.println("Serviço:  " + servicos);
-         System.out.println("Dia agendado: " + novoDiaSemana);
-         System.out.println("Horário agendado: " + novoHorario);
-         }
+            //aqui mesmo o agen sendo cancelado, está retornando a lista com os dados. E não é p retornar.
+            //Não consegui ajeitar! :/
+
+            System.out.println("==========================");
+            //clientes agendados inseridos pelo scanner
+            System.out.println("Dados do agendamento:");
+        for(Agenda agendamentos : listaAgendamentos){
+                System.out.println("Animal: " + agendamentos.getNome());
+                System.out.println("Espécie: " + agendamentos.getEspecie());
+                System.out.println("Raça: " + agendamentos.getRaça());
+                System.out.println("Data nascimento: " + agendamentos.getDataNascimento());
+                System.out.println("proprietário: " + agendamentos.getProprietario());
+                System.out.println("telefone: " + agendamentos.getTelefone());
+                System.out.println("Serviço:  " + servicos);
+                System.out.println("Dia agendado: " + novoDiaSemana);
+                System.out.println("Horário agendado: " + novoHorario);
+            }
     
         }
-        
-        //clientes pets já cadastrados no sistema
-        Cachorro ca1 = new Cachorro("Caramelo", "Cachorro", "viralata", "20/9/21","Patricia", "8199413987");
-        Cachorro ca2 = new Cachorro("Tufo", "Cachorro", "Bulldog", "8/3/17","Vinicius", "8199413987");
-        Gato ga1 = new Gato("Lala", "Gato", "SRD", "8/05/18","Layla", "8199413987");
-        Gato ga2 = new Gato("Chica", "Gato", "SRD", "15/08/17","Luzi", "8199413987");
-        listaCachorros.add(ca1);
-        listaCachorros.add(ca2);
-        listaGatos.add(ga1);
-        listaGatos.add(ga2);
+            //clientes pets já cadastrados no sistema
+            Cachorro ca1 = new Cachorro("Caramelo", "Cachorro", "viralata", "20/9/21","Patricia", "8199413987");
+            Cachorro ca2 = new Cachorro("Tufo", "Cachorro", "Bulldog", "8/3/17","Vinicius", "8199413987");
+            Gato ga1 = new Gato("Lala", "Gato", "SRD", "8/05/18","Layla", "8199413987");
+            Gato ga2 = new Gato("Chica", "Gato", "SRD", "15/08/17","Luzi", "8199413987");
+            listaCachorros.add(ca1);
+            listaCachorros.add(ca2);
+            listaGatos.add(ga1);
+            listaGatos.add(ga2);
 
-        //adicionando produtos ao estoque
-        System.out.println("===========================");
-        System.out.println("======Nossos produtos======");
-        Produto item1 = new Produto( "Bolinha ",  6.0,   20);
-        Produto item2 = new Produto( "Varinha ",  8.0,   30);
-        Produto item3 = new Produto( "Osso de plastico ", 10.0,  10);
-        brinquedos.add(item1);
-        brinquedos.add(item2);
-        brinquedos.add(item3);
+            //adicionando produtos ao estoque
+            System.out.println("===========================");
+            System.out.println("======Nossos produtos======");
+            Produto item1 = new Produto( "Bolinha ",  6.0,   20);
+            Produto item2 = new Produto( "Varinha ",  8.0,   30);
+            Produto item3 = new Produto( "Osso de plastico ", 10.0,  10);
+            brinquedos.add(item1);
+            brinquedos.add(item2);
+            brinquedos.add(item3);
 
-        Produto item4 = new Produto( "Ração ",   150.0,  30);
-        Produto item5 = new Produto( "Petisco ",  5.0,  40);
-        alimentos.add(item4);
-        alimentos.add(item5);
+            Produto item4 = new Produto( "Ração ",   150.0,  30);
+            Produto item5 = new Produto( "Petisco ",  5.0,  40);
+            alimentos.add(item4);
+            alimentos.add(item5);
         
 
         Produto produtos1 = new Produto("Caminha", 200.0,  5);
@@ -208,8 +210,6 @@ public class PetShop {
             System.out.println("Produto fora de estoque.");
         }
 
-
-
         System.out.println("===========================");
        //lista dos clientes pets já cadatrados no sistema
         System.out.println("====Lista dos clientes pets:==== ");
@@ -228,7 +228,7 @@ public class PetShop {
         }
      
         System.out.println("=======================");
-    
+        meuLogger.logger.info("Lista atualizada " + listaAgendamentos);
     }
 
     
